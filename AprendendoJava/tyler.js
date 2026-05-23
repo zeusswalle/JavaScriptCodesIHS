ac464ea5877a198d5c0e476e1a723df3c31bcb0f
david <140751426+zeusswalle@users.noreply.github.com>
2026-05-23 15:51:59 -0300

    atualizando geral

---
+++ AprendendoJava/tyler.js
@@ -0,0 +1,22 @@
+let name = "Tyler, the Creator😎";
+let realName = "Tyler Gregory Okonma";
+let idade = 35;
+let cidade = "Los Angeles🌇";
+let trabalho = "Produtor musical🎧";
+let ouvintes = 40135380;
+
+
+
+
+console.log(`Nome Artistico: ${name}`);
+console.log(`Nome real: ${realName}`)
+console.log(`Idade: ${idade}🧍`);
+console.log(`Sua cidade: ${cidade}`);
+console.log(`Seu trabalho: ${trabalho}`);
+console.log(`Ouvintes: ${ouvintes}🤯`);
+
+if ( ouvintes > 1000000){
+    console.log(`Esse cara é famoso e tem 😎${ouvintes}😎 ouvintes no Spotify.`)
+} else {
+    console.log("Esse cara não é famoso.")
+}
